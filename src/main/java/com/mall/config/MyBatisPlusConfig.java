090c454a8c9fd3caa5633a5c2f7a5ce06793bdef
ProerLy <1442404089@qq.com>
2026-04-29 14:52:43 +0800

    xg

--- src/main/java/com/mall/config/MyBatisPlusConfig.java
+++
@@ -1,9 +0,0 @@
-package com.mall.config;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@MapperScan("com.mall.repository")
-public class MyBatisPlusConfig {
-}
--- src/main/java/com/mall/repository/GoodsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mall.repository;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mall.entity.Goods;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface GoodsRepository extends BaseMapper<Goods> {
-}
